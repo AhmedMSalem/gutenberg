33ea5b1315632b1e1cca2152511a5322fdb4337b
Andrew Duthie <andrew@andrewduthie.com>
2019-07-12 16:32:01 -0400

    Notices: Correct typo in WPNoticeAction onClick callback property

--- packages/notices/src/store/selectors.js
+++ packages/notices/src/store/selectors.js
@@ -41,11 +41,11 @@ const DEFAULT_NOTICES = [];
 /**
  * @typedef {Object} WPNoticeAction Object describing a user action option associated with a notice.
  *
- * @property {string}    label    Message to use as action label.
- * @property {?string}   url      Optional URL of resource if action incurs
- *                                browser navigation.
- * @property {?Function} onClick  Optional function to invoke when action is
- *                                triggered by user.
+ * @property {string}    label   Message to use as action label.
+ * @property {?string}   url     Optional URL of resource if action incurs
+ *                               browser navigation.
+ * @property {?Function} onClick Optional function to invoke when action is
+ *                               triggered by user.
  *
  */
 
